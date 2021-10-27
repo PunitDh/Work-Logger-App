8a2f0388b194c959f15bc64bd22ef7ce7a841ffa
Punit Dharmadhikari <punit.dharmadhikari@lexicondigital.com.au>
2021-10-28 07:35:02 +1100

    Refactor CSS, apply proxy to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
-import dateFormat from "dateformat";
 import { request } from "./request";
+import Task from './components/Task';
 
 function App() {
   const [task, setTask] = useState("");
@@ -39,23 +39,20 @@ function App() {
       {notification ?
         (<div className="bg-green-300 text-green-700 border-2 border-green-700 notification">{notification}</div>) : null}
       {error ? (<div className="bg-red-300 text-red-700 border-2 border-red-700 notification">{error}</div>) : null}
-      <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white">
-        <h1 className="text-4xl mb-4 w-8/12 py-4 rounded border-2">Work Logger App</h1>
-        <div className="grid grid-cols-2 gap-4 text-xl w-8/12 py-4 rounded border-2">
+      <div className="logger-container">
+        <h1 className="logger-header">Work Logger App</h1>
+        <div className="logger-box">
           <div className="text-blue-500">Task</div>
           <div className="text-blue-500">Timestamp</div>
           {
             tasks && tasks.map((t, idx) => (
-              <React.Fragment key={idx}>
-                <div className="text-white text-left ml-4">{t.name}</div>
-                <div className="text-white">{dateFormat(t.timestamp, "ddd, mmm dS, yyyy, h:MM TT")}</div>
-              </React.Fragment>
+              <Task key={idx} task={t} />
             ))
           }
         </div>
-        <form className="flex text-4xl mb-4 w-8/12 py-4" onSubmit={handleSubmit}>
-          <input type="text" autoFocus={true} placeholder="Add New Task" className="text-black text-2xl mx-4 pl-2 border-4 w-8/12 border-blue-500 rounded" value={task} onChange={handleChange} />
-          <input type="submit" className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-lg" value="Add New" />
+        <form className="logger-form" onSubmit={handleSubmit}>
+          <input type="text" autoFocus={true} placeholder="Add New Task" className="input-box" value={task} onChange={handleChange} />
+          <input type="submit" className="submit-button" value="Add New" />
         </form>
       </div>
     </div>
